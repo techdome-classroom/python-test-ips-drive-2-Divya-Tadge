bc9b809021d53956f8173352a9f244563bfbc2cb
Divya Tadge <tadgedivya2003@gmail.com>
2024-04-26 16:13:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,21 @@ class Solution(object):
         """
         pass
     
-
+class Solution(object):
+  
+    def isValid(self, s):
+        stack = [] 
+        for c in s:
+            if c in '([{': 
+                stack.append(c) 
+            else:
+                if not stack or \
+                    (c == ')' and stack[-1] != '(') or \
+                    (c == '}' and stack[-1] != '{') or \
+                    (c == ']' and stack[-1] != '['):
+                    return False 
+                stack.pop()
+        return not stack
 
 
   
